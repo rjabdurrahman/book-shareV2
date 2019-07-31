5d9591f047ddecb996ec935761c4168e6641df9c
rjabdurrahman <rj.abdurrahman@gmail.com>
2019-07-31 22:39:43 +0600

    Added: Sate manage in SignUP

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react'
 import PrimaryNav from '../components/PrimaryNav'
 
 export class Register extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fullName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
             <div className="register-div">
                 <PrimaryNav></PrimaryNav>
-                <section className="section-book">
+                <form onSubmit={this.handleSubmit} className="section-book">
                     <div className="row">
                         <div className="book">
                             <div className="book__form">
@@ -21,29 +42,29 @@ export class Register extends Component {
                                         <a href="data.html" className=" btn-text">Sign In with Google →</a>
                                     </div>
                                     <div className="form__group">
-                                        <input type="text" className="form__input" placeholder="Full Name" id="name" required />
+                                        <input type="text" className="form__input" placeholder="Full Name" name="fullName" value={this.state.fullName} onChange={this.handleChange} required />
                                         <label htmlFor="name" className="form__label">Full Name</label>
                                     </div>
                                     <div className="form__group">
-                                        <input type="email" className="form__input" placeholder="Email Address" id="email" required />
+                                        <input type="email" className="form__input" placeholder="Email Address" name="email" value={this.state.email} onChange={this.handleChange} required />
                                         <label htmlFor="email" className="form__label">Email Address</label>
                                     </div>
                                     <div className="form__group">
-                                        <input type="password" className="form__input" placeholder="Password" id="password" required />
+                                        <input type="password" className="form__input" placeholder="Password" name="password" value={this.state.password} onChange={this.handleChange} required />
                                         <label htmlFor="password" className="form__label">Password</label>
                                     </div>
                                     <div className="form__group">
-                                        <input type="password" className="form__input" placeholder="Confirm Password" id="password" required />
+                                        <input type="password" className="form__input" placeholder="Confirm Password" name="confirmPassword" value={this.state.confirmPassword} onChange={this.handleChange} required />
                                         <label htmlFor="password" className="form__label"> Confirm Password</label>
                                     </div>
                                     <div className="form__group">
-                                        <button className="btn btn--green">Log in  →</button>
+                                        <input type="submit" className="btn btn--green" value="Sign UP  →" />
                                     </div>
                                 </form>
                             </div>
                         </div>
                     </div>
-                </section>
+                </form>
 
             </div>
         )
