fc46c22d8d4039f5ea0a09f5ebcc9091ac5cdca4
rjabdurrahman <md.rahman0296@gmail.com>
2019-08-25 19:28:33 +0600

    Added: Book List

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react'
 import './BookList.css';
+import axios from 'axios';
 
 export class BookList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            bookList: []
+        }
+    }
+
+    componentWillMount() {
+        axios.get('http://localhost:5000/api/add-book')
+            .then(res => this.setState({ bookList: res.data }))
+            .catch(err => console.log(err));
+    }
     render() {
+        const allBooks = this.state.bookList.map(book => (
+            <div className="col-sm-3">
+                <div className="thumb-wrapper">
+                    <span className="wish-icon"><i className="fa fa-heart-o" /></span>
+                    <div className="img-box">
+                        <img src="https://static01.nyt.com/images/2019/08/07/books/06bookalbinati1/06bookalbinati1-jumbo.jpg" className="img-responsive img-fluid" alt="" />
+                    </div>
+                    <div className="thumb-content">
+                        <h4>{book.title}</h4>
+                        <div className="star-rating">
+                            <ul className="list-inline">
+                                <li className="list-inline-item"><i className="fa fa-star" /></li>
+                                <li className="list-inline-item"><i className="fa fa-star" /></li>
+                                <li className="list-inline-item"><i className="fa fa-star" /></li>
+                                <li className="list-inline-item"><i className="fa fa-star" /></li>
+                                <li className="list-inline-item"><i className="fa fa-star-half-o" /></li>
+                            </ul>
+                        </div>
+                        <a href="data.html" className="btn btn-primary">Want To Read <i class="fas fa-angle-down"></i></a>
+                    </div>
+                </div>
+            </div>
+        ));
         return (
             <div className="container">
                 <div className="row">
@@ -19,70 +55,7 @@ export class BookList extends Component {
                             <div className="carousel-inner">
                                 <div className="item carousel-item active">
                                     <div className="row">
-                                        <div className="col-sm-3">
-                                            <div className="thumb-wrapper">
-                                                <span className="wish-icon"><i className="fa fa-heart-o" /></span>
-                                                <div className="img-box">
-                                                    <img src="https://static01.nyt.com/images/2019/08/07/books/06bookalbinati1/06bookalbinati1-jumbo.jpg" className="img-responsive img-fluid" alt="" />
-                                                </div>
-                                                <div className="thumb-content">
-                                                    <h4>Sample Book 1</h4>
-                                                    <div className="star-rating">
-                                                        <ul className="list-inline">
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star-half-o" /></li>
-                                                        </ul>
-                                                    </div>
-                                                    <a href="data.html" className="btn btn-primary">Want To Read <i class="fas fa-angle-down"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-sm-3">
-                                            <div className="thumb-wrapper">
-                                                <span className="wish-icon"><i className="fa fa-heart-o" /></span>
-                                                <div className="img-box">
-                                                    <img src="https://static01.nyt.com/images/2019/08/07/books/06bookalbinati1/06bookalbinati1-jumbo.jpg" className="img-responsive img-fluid" alt="" />
-                                                </div>
-                                                <div className="thumb-content">
-                                                    <h4>Sample Book 2</h4>
-                                                    <div className="star-rating">
-                                                        <ul className="list-inline">
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star-half-o" /></li>
-                                                        </ul>
-                                                    </div>
-                                                    <a href="data.html" className="btn btn-primary">Want to Read <i class="fas fa-angle-down"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="col-sm-3">
-                                            <div className="thumb-wrapper">
-                                                <span className="wish-icon"><i className="fa fa-heart-o" /></span>
-                                                <div className="img-box">
-                                                    <img src="https://static01.nyt.com/images/2019/08/07/books/06bookalbinati1/06bookalbinati1-jumbo.jpg" className="img-responsive img-fluid" alt="" />
-                                                </div>
-                                                <div className="thumb-content">
-                                                    <h4>Sample Book 3</h4>
-                                                    <div className="star-rating">
-                                                        <ul className="list-inline">
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star" /></li>
-                                                            <li className="list-inline-item"><i className="fa fa-star-half-o" /></li>
-                                                        </ul>
-                                                    </div>
-                                                    <a href="data.html" className="btn btn-primary">Want to Read <i class="fas fa-angle-down"></i></a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        {/* End */}
+                                        {allBooks}
                                     </div>
                                 </div>
                                 {/* Another */}
